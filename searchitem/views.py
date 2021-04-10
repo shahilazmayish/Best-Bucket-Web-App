4bcd73f663d87ca6a4d3b57fceeba3d1fa51a5ee
shahilazmayish <shahilazmayish@gmail.com>
2021-04-10 22:56:07 +0600

    well stablished database

--- searchitem/views.py
+++ searchitem/views.py
@@ -6,10 +6,14 @@ def homepage(request):
 
     return render(request, "homepage.html", {"name": "Best Bucket"})
 
-
-
 def searchproduct(request):
     query = str(request.GET['q'])
     product=Product.objects.filter(name__icontains=query)
-
     return render(request, "result.html", {'product': product})
+
+# def searchproductcat(request):
+#     query = str(request.GET['q'])
+#     query1 = str(request.GET['id'])
+#     product=Product.objects.filter(name__icontains=query).exclude(name__icontains=query1)
+
+#     return render(request, "result.html", {'product': product})
